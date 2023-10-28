4480082e6397d68b763f8cab7dc423b6bd7b9083
Jesse Shawl <jesse@jesse.sh>
2023-10-28 13:43:15 -0500

    Attachments (#12)
    related to #11

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const core = require('@actions/core');
-const { email } = require('@cinotify/js')
+const mime = require('mime');
+const { email } = require('@cinotify/js');
+const {readFileSync} = require('fs');
 
 try {
     const to = core.getInput('to');
     const subject = core.getInput('subject');
     const body = core.getInput('body');
-    email({
-        to,
-        subject,
-        body
-    }).then(() => {
+    const payload = {to, subject, body};
+
+    const attachmentPath = core.getInput('attachment');
+    if (attachmentPath) {
+        const file = readFileSync(attachmentPath);
+        const attachment = {
+            filename: attachmentPath,
+            type: mime.getType(attachmentPath),
+            content: file.toString('base64')
+        }
+        payload.attachments = [attachment];
+    }
+
+    email(payload).then(() => {
         core.setOutput("status", "ok")
     }).catch(err => {
         core.setFailed(err.message)
