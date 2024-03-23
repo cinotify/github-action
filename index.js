0d0de52185425aeb4df45f4e0b911f3e6261b335
Jesse Shawl <jesse@jesse.sh>
2024-03-23 11:42:28 -0500

    add ci / refactor (#20)

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-const core = require('@actions/core');
-const mime = require('mime');
-const {readFileSync} = require('fs');
+import { notify } from "./notify.js";
 
-try {
-    const to = core.getInput('to');
-    const subject = core.getInput('subject');
-    const body = core.getInput('body');
-    const type = core.getInput('type');
-    const payload = {to, subject, body, type};
-
-    const attachmentPath = core.getInput('attachment');
-    if (attachmentPath) {
-        const file = readFileSync(attachmentPath);
-        const attachment = {
-            filename: attachmentPath,
-            type: mime.getType(attachmentPath),
-            content: file.toString('base64')
-        }
-        payload.attachments = [attachment];
-    }
-
-    fetch('https://www.cinotify.cc/api/notify', {
-        body: JSON.stringify(payload),
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': `@cinotify/github-action@${process.env['GITHUB_ACTION_REF']}`
-        },
-        method: 'POST'
-    }).then(() => {
-        core.setOutput("status", "ok")
-    }).catch(err => {
-        core.setFailed(err.message)
-    })
-} catch (error) {
-    core.setFailed(error.message);
-}
+notify();
